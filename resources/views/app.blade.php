40ec7c47a14bd2ec5286556cda6e6f1c6a546f2b
r-ume <baymax.blackandwhite@gmail.com>
2017-03-05 08:36:19 +0000

    flash message can get closed automatically

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -68,5 +68,9 @@
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    <script>
+        $('div.alert').not('.alert-important').delay(3000).slideUp(300);
+    </script>
 </body>
 </html>
