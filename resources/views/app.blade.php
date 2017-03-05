9b0eaece8cf238857088ed9b775f25d86150a904
r-ume <baymax.blackandwhite@gmail.com>
2017-03-05 08:31:48 +0000

    flash message can get closed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -57,6 +57,7 @@
     <div class = "container">
         @if(Session::has('flash_message'))
             <div class = "alert alert-success">
+                <button type = "button" class = "close" data-dismiss = "alert" aria-hidden="true">&times;</button>
                 {{ Session::get('flash_message')}}
             </div>
         @endif
