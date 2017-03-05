5d67a3ea28ecf1edf2bd460affa9dac2322f7ac7
r-ume <baymax.blackandwhite@gmail.com>
2017-03-05 08:48:41 +0000

    created wishtimes and categories table

---
+++ database/migrations/2017_03_05_084634_create_categories_wishtimes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesWishtimesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('categories_wishtimes', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('category_id')->unsigned()->index();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->integer('wishtimes_id')->unsigned()->index();
+            $table->foreign('wishtimes_id')->references('id')->on('wishtimes')->onDelete('cascade');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('categories_feeds');
+    }
+
+}
