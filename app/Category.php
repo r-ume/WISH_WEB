6c59275794dc0a908f2e735bb1bcbc00ce14cb6e
r-ume <baymax.blackandwhite@gmail.com>
2017-03-05 09:14:49 +0000

    made relationship between category and wishtimes

--- app/Category.php
+++ app/Category.php
@@ -4,9 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model {
 
-	protected $table = '';
+	protected $table = 'categories';
     
-    protected $fillable = [];
+    protected $fillable = ['name'];
     
     public function events(){
         return $this->belongsToMany('App\Event')->withTimestamps();
--- app/Wishtimes.php
+++ app/Wishtimes.php
@@ -11,4 +11,12 @@ class Wishtimes extends Model {
     public function user(){
         return $this->belongsTo('App\User');
     }
+    
+    public function categories(){
+        return $this->belongsToMany('App\Category', 'categories_wishtimes', 'wishtimes_id', 'category_id');
+    }
+    
+    public function getCategoriesListAttribute(){
+        return $this->categories->lists('id');
+    }
 }
