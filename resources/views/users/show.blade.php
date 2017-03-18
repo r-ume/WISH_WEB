14968ca7e654fe92c64d6edd2db429568fd45da6
r-ume <baymax.blackandwhite@gmail.com>
2017-03-18 09:47:00 +0000

    made profile page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -35,92 +34,91 @@
 
 </head>
 <body>
-
-<div class="ui fixed inverted menu">
-    <div class="ui container">
-        <a href="/myprofile" class="header item">
-            <img class="logo" src="{{ asset($user->image) }}">
-            WISH_WEB
-        </a>
-        <a href="/myprofile" class="item">Home</a>
-        <a href="/tweets" class="item">Tweet</a>
-        <a href="/events" class="item">Events</a>
-        <a href="/wishtimes" class="item">Wishtimes</a>
+    <div class="ui fixed inverted menu">
+        <div class="ui container">
+            <a href="/myprofile" class="header item">
+                <img class="logo" src="{{ asset($user->image) }}">
+                WISH_WEB
+            </a>
+            <a href="/myprofile" class="item">Home</a>
+            <a href="/tweets" class="item">Tweet</a>
+            <a href="/events" class="item">Events</a>
+            <a href="/wishtimes" class="item">Wishtimes</a>
+        </div>
     </div>
-</div>
 
-<div class="ui main text container">
-    <div class="ui one cards">
-        <div class="ui card">
-            <div class="ui move reveal image">
-                <div class="ui blurring inverted dimmer">
-                    <div class="content">
-                        <div class="center">
-                            <div class="ui teal button">Add Friend</div>
+    <div class="ui main text container">
+        <div class="ui one cards">
+            <div class="ui card">
+                <div class="ui move reveal image">
+                    <div class="ui blurring inverted dimmer">
+                        <div class="content">
+                            <div class="center">
+                                <div class="ui teal button">Add Friend</div>
+                            </div>
                         </div>
                     </div>
+                    <img src="{{ asset($user->image) }}" style = "width: 684px; height: 400px;">
                 </div>
-                <img src="{{ asset($user->image) }}" style = "width: 684px; height: 400px;">
-            </div>
-            <div class="content">
-                <div class="header">{{ $user->first_name }} {{ $user->last_name }}</div>
-                <div class="meta">
-                    <a class="group">Floor: {{ $user->floor }}</a>
-                    @foreach($user->roles as $role)
-                        <p>Role: {{ $role->role }}</p>
-                    @endforeach
+                <div class="content">
+                    <div class="header">{{ $user->first_name }} {{ $user->last_name }}</div>
+                    <div class="meta">
+                        <a class="group">Floor: {{ $user->floor }}</a>
+                        @foreach($user->roles as $role)
+                            <p>Role: {{ $role->role }}</p>
+                        @endforeach
+                    </div>
+                    <div class="description">5階のRAです！</div>
                 </div>
-                <div class="description">5階のRAです！</div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="ui inverted vertical footer segment">
-    <div class="ui center aligned container">
-        <div class="ui stackable inverted divided grid">
-            <div class="three wide column">
-                <h4 class="ui inverted header">Group 1</h4>
-                <div class="ui inverted link list">
-                    <a href="#" class="item">Link One</a>
-                    <a href="#" class="item">Link Two</a>
-                    <a href="#" class="item">Link Three</a>
-                    <a href="#" class="item">Link Four</a>
+    <div class="ui inverted vertical footer segment">
+        <div class="ui center aligned container">
+            <div class="ui stackable inverted divided grid">
+                <div class="three wide column">
+                    <h4 class="ui inverted header">Group 1</h4>
+                    <div class="ui inverted link list">
+                        <a href="#" class="item">Link One</a>
+                        <a href="#" class="item">Link Two</a>
+                        <a href="#" class="item">Link Three</a>
+                        <a href="#" class="item">Link Four</a>
+                    </div>
+                </div>
+                <div class="three wide column">
+                    <h4 class="ui inverted header">Group 2</h4>
+                    <div class="ui inverted link list">
+                        <a href="#" class="item">Link One</a>
+                        <a href="#" class="item">Link Two</a>
+                        <a href="#" class="item">Link Three</a>
+                        <a href="#" class="item">Link Four</a>
+                    </div>
+                </div>
+                <div class="three wide column">
+                    <h4 class="ui inverted header">Group 3</h4>
+                    <div class="ui inverted link list">
+                        <a href="#" class="item">Link One</a>
+                        <a href="#" class="item">Link Two</a>
+                        <a href="#" class="item">Link Three</a>
+                        <a href="#" class="item">Link Four</a>
+                    </div>
+                </div>
+                <div class="seven wide column">
+                    <h4 class="ui inverted header">Footer Header</h4>
+                    <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
                 </div>
             </div>
-            <div class="three wide column">
-                <h4 class="ui inverted header">Group 2</h4>
-                <div class="ui inverted link list">
-                    <a href="#" class="item">Link One</a>
-                    <a href="#" class="item">Link Two</a>
-                    <a href="#" class="item">Link Three</a>
-                    <a href="#" class="item">Link Four</a>
-                </div>
+            <div class="ui inverted section divider"></div>
+            <img src="assets/images/logo.png" class="ui centered mini image">
+            <div class="ui horizontal inverted small divided link list">
+                <a class="item" href="#">Site Map</a>
+                <a class="item" href="#">Contact Us</a>
+                <a class="item" href="#">Terms and Conditions</a>
+                <a class="item" href="#">Privacy Policy</a>
             </div>
-            <div class="three wide column">
-                <h4 class="ui inverted header">Group 3</h4>
-                <div class="ui inverted link list">
-                    <a href="#" class="item">Link One</a>
-                    <a href="#" class="item">Link Two</a>
-                    <a href="#" class="item">Link Three</a>
-                    <a href="#" class="item">Link Four</a>
-                </div>
-            </div>
-            <div class="seven wide column">
-                <h4 class="ui inverted header">Footer Header</h4>
-                <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-            </div>
-        </div>
-        <div class="ui inverted section divider"></div>
-        <img src="assets/images/logo.png" class="ui centered mini image">
-        <div class="ui horizontal inverted small divided link list">
-            <a class="item" href="#">Site Map</a>
-            <a class="item" href="#">Contact Us</a>
-            <a class="item" href="#">Terms and Conditions</a>
-            <a class="item" href="#">Privacy Policy</a>
         </div>
     </div>
-</div>
 </body>
 
 </html>
