a4020a79da4c881901b552ff4034129bf2677b21
r-ume <baymax.blackandwhite@gmail.com>
2017-03-20 06:01:24 +0000

    made wishtiems show page

--- resources/views/wishtimes/show.blade.php
+++ resources/views/wishtimes/show.blade.php
@@ -1,25 +1,112 @@
-@extends('app')
 
-@section('content')
-    <h1>{{$wishtimes->title}}</h1>
-    <h2>{{$wishtimes->content}}</h2>
-    <span><img src="{{ asset($wishtimes->image) }}" /></span>
+    {{--{!! Form::model($wishtimes, ['method' => 'GET', 'url' => 'wishtimes/edit/'.$wishtimes->id]) !!}--}}
+        {{--<button type="submit" class="btn btn-primary">edit</button>--}}
+    {{--{!! Form::close() !!}--}}
 
-    {!! Form::model($wishtimes, ['method' => 'GET', 'url' => 'wishtimes/edit/'.$wishtimes->id]) !!}
-        <button type="submit" class="btn btn-primary">edit</button>
-    {!! Form::close() !!}
+    {{--{!! Form::model($wishtimes, ['method' => 'DELETE', 'url' => 'wishtimes/'.$wishtimes->id]) !!}--}}
+        {{--<button type="submit" class="btn btn-danger"--}}
+            {{--onclick = 'return confirm("Are you sure that you would like to delete this circle?");'>delete</button>--}}
+    {{--{!! Form::close() !!}--}}
 
-    {!! Form::model($wishtimes, ['method' => 'DELETE', 'url' => 'wishtimes/'.$wishtimes->id]) !!}
-        <button type="submit" class="btn btn-danger"
-            onclick = 'return confirm("Are you sure that you would like to delete this circle?");'>delete</button>
-    {!! Form::close() !!}
+    {{--@unless($wishtimes->categories->isEmpty())--}}
+        {{--<h5>Categories: </h5>--}}
+        {{--<ul>--}}
+            {{--@foreach($wishtimes->categories as $category)--}}
+                {{--<li>{{$category->name}}</li>--}}
+            {{--@endforeach--}}
+        {{--</ul>--}}
+    {{--@endunless--}}
+{{--@endsection--}}
 
-    @unless($wishtimes->categories->isEmpty())
-        <h5>Categories: </h5>
-        <ul>
-            @foreach($wishtimes->categories as $category)
-                <li>{{$category->name}}</li>
-            @endforeach
-        </ul>
-    @endunless
-@endsection
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+        <link rel = "stylesheet" type = "text/css" href = "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.css" />
+        <script type ="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+        <script type="text/javascript" src="javascript/easyng.js"></script>
+    </head>
+<body>
+    <!-- Navbar fixed top area -->
+    <div class="ui large top hidden menu">
+        <div class="ui container">
+            <a href="/myprofile" class="header item">
+                <img class="logo" src="{{ asset($user->image) }}">
+            </a>
+            <a class = "item" href = "/">WISH_WEB</a>
+            <a class="item" href = "/myprofile">MyProfile</a>
+            <a class="item" href = "/events">Events</a>
+            <a class="item" href = "/wishtimes">Wishtimes</a>
+            <a class="item" href = "/tweets">Tweets</a>
+            <div class="right menu">
+                <div class="item">
+                    <a class="ui button">Log in</a>
+                </div>
+                <div class="item">
+                    <a class="ui primary button">Sign Up</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Content area -->
+    <div class="ui container content">
+        <div class="ui stackable doubling grid">
+            <!-- Middle Content -->
+            <div class="sixteen wide column">
+                <div class="ui horizontal divider"><h2>{{ $wishtimes->title }}</h2></div>
+                <div align="center">
+                    <p>{{ $wishtimes->content }}</p>
+                    {{--<blockquote><h3>"Berkarya, Bersama, Berjaya"</h3></blockquote>--}}
+                </div>
+                <br>
+                {{--<div class="ui horizontal divider">Sample History</div>--}}
+                {{--<p></p>--}}
+                <br>
+                <div class="ui horizontal divider">Photos</div>
+                <!-- Slider Container -->
+                <div class="ui container">
+                    <div class="ui centered stackable doubling grid">
+                        <div class="eight wide column">
+                            <div class="owl-carousel" id="multiple-slider">
+                                <div class="item"><img src="{{ asset($user->image) }}" style = "width: 130px; height: "130px"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer area -->
+    <div class="ui inverted vertical footer segment">
+        <div class="ui container">
+            <div class="ui stackable inverted divided equal height stackable grid">
+                <div class="three wide column">
+                    <h4 class="ui inverted header">WISH_WEBについて</h4>
+                    <div class="ui inverted link list">
+                        <a href="#" class="item">困ったら、RAに連絡を</a>
+                    </div>
+                </div>
+                <div class="three wide column">
+                    <h4 class="ui inverted header">Services</h4>
+                    <div class="ui inverted link list">
+                        <a href="#" class="item">マイプロフィール</a>
+                        <a href="#" class="item">Wishtimes</a>
+                        <a href="#" class="item">イベント</a>
+                        <a href="#" class="item">つぶやきWISH</a>
+                    </div>
+                </div>
+                <div class="seven wide column">
+                    <h4 class="ui inverted header">いいね！</h4>
+                    <p>気楽にゆるーく、まあ、何回も失敗してもいいんじゃない？死ぬわけじゃないじゃないし</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
