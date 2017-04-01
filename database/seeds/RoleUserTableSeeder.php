5dd9882cacf4ac5541c36539382eb9db08b69976
r-ume <baymax.blackandwhite@gmail.com>
2017-04-01 09:09:03 +0000

    added test users

--- database/seeds/RoleUserTableSeeder.php
+++ database/seeds/RoleUserTableSeeder.php
@@ -18,8 +18,28 @@ class RoleUserTableSeeder extends Seeder{
             ],
             [
                 'user_id' => '3',
-                'role_id' => '2'
-            ]
+                'role_id' => '1'
+            ],
+            [
+                'user_id' => '4',
+                'role_id' => '1',
+            ],
+            [
+                'user_id' => '5',
+                'role_id' => '2',
+            ],
+            [
+                'user_id' => '6',
+                'role_id' => '2',
+            ],
+            [
+                'user_id' => '7',
+                'role_id' => '2',
+            ],
+            [
+                'user_id' => '8',
+                'role_id' => '2',
+            ],
         ]);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,30 +13,80 @@ class UsersTableSeeder extends Seeder{
                 'last_name' => 'umeki',
                 'sex' => 'male',
                 'language_id' => '1',
-                'image' => 'test.jpg',
+                'image' => 'uploads/RAs/ryo.jpg',
                 'floor' => '5',
                 'email' => 'ryo@umeki.com',
                 'password' => bcrypt('ryoumeki'),
+            ],
+            [
+                'first_name' => 'cheryon',
+                'last_name'  => 'park',
+                'sex' => 'female',
+                'language_id' => '4',
+                'image' => 'uploads/RAs/cheryon.jpg',
+                'floor' => 5,
+                'email' => 'cheryon@park.com',
+                'password' => bcrypt('cheryon'),
             ],
             [
                 'first_name' => 'yutaro',
                 'last_name' => 'kurahashi',
                 'sex' => 'male',
                 'language_id' => '1',
-                'image' => 'test.jpg',
+                'image' => 'uploads/RAs/yutaro.jpg',
                 'floor' => '2',
                 'email' => 'yutaro@kurashi.com',
                 'password' => bcrypt('yutarokurahashi')
             ],
             [
-                'first_name' => 'natumi',
-                'last_name' => 'higashige',
+                'first_name' => 'mari',
+                'last_name' => 'mizutani',
                 'sex' => 'female',
                 'language_id' => '2',
-                'image' => 'test.jpg',
+                'image' => 'uploads/RAs/mari.jpg',
                 'floor' => '8',
                 'email' => 'mari@mizutani.com',
                 'password' => bcrypt('marimizutani')
+            ],
+            [
+                'first_name' => '吉田',
+                'last_name' => 'こうへい',
+                'sex' => 'male',
+                'language_id' => '1',
+                'image' => 'uploads/RAs/kouhei.jpg',
+                'floor' => '5',
+                'email' => 'yoshida@kouhei.com',
+                'password' => bcrypt('yoshidakouhei')
+            ],
+            [
+                'first_name' => 'mizyu',
+                'last_name' => 'unknown',
+                'sex' => 'female',
+                'language_id' => '3',
+                'image' => 'uploads/RAs/mizyu.jpg',
+                'floor' => '10',
+                'email' => 'mizyu@unknown.com',
+                'password' => bcrypt('mizyuunknown')
+            ],
+            [
+                'first_name' => 'taichi',
+                'last_name' => 'murase',
+                'sex' => 'male',
+                'language_id' => '2',
+                'image' => 'uploads/RAs/taichi.jpg',
+                'floor' => '7',
+                'email' => 'taichi@murase.com',
+                'password' => bcrypt('taichimurase')
+            ],
+            [
+                'first_name' => 'michel',
+                'last_name' => 'unknown',
+                'sex' => 'male',
+                'language_id' => '1',
+                'image' => 'uploads/RAs/michel.jpg',
+                'floor' => '3',
+                'email' => 'michel@unknown.com',
+                'password' => bcrypt('michelunknown')
             ]
         ]);
     }
