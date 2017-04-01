03028fbd517967bc4ec1d57ef2fd5cde56023419
r-ume <baymax.blackandwhite@gmail.com>
2017-04-01 13:27:45 +0000

    recent events are displayed in the sidebar in the index page

--- app/Http/Controllers/FullCalendarController.php
+++ app/Http/Controllers/FullCalendarController.php
@@ -6,19 +6,39 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use \Calendar as Calendar;
+use \Auth as Auth;
+use App\Event;
+use App\Wishtimes;
 
-class FullCalendarController extends Controller {
+class FullCalendarController extends Controller{
 
 	public function index(){
 
+	    $user = Auth::user();
         $events = [];
 
         $events[] = Calendar::event(
-            "Event one", // event title
+            "BBQ", // event title
             true, // full day event
-            '2017-01-01', // start time
-            '2017-01-02', // end time
-            0
+            new \DateTime('2017-04-16'), // start time
+            new \DateTime('2017-04-16'), // end time
+            1
+        );
+
+        $events[] = Calendar::event(
+            "RA 会議",
+            true,
+            new \DateTime('2017-04-03'),
+            new \DateTime('2017-04-03'),
+            2
+        );
+
+        $events[] = Calendar::event(
+            "5階と9階の合コン",
+            true,
+            new \DateTime('2017-04-04'),
+            new \DateTime('2017-04-04'),
+            3
         );
 
         $calendar = Calendar::addEvents($events)
@@ -26,6 +46,10 @@ class FullCalendarController extends Controller {
                     'firstDay' => 1
                 ]);
 
-        return view('calendar.index', compact('calendar'));
+        $events = Event::orderBy('created_at', 'DESC')->get();
+
+//        $joiningEvents = Event::orderBy
+
+        return view('calendar.index', compact('calendar', 'user', 'events'));
     }
 }
