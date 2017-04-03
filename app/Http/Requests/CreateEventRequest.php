abc493276d5d0bfbd551db79fe762920874264e3
r-ume <baymax.blackandwhite@gmail.com>
2017-04-03 16:49:52 +0000

    added new properties in request validation

--- app/Http/Requests/CreateEventRequest.php
+++ app/Http/Requests/CreateEventRequest.php
@@ -4,26 +4,28 @@ use App\Http\Requests\Request;
 
 class CreateEventRequest extends Request {
 
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		return true;
-	}
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(){
+        return true;
+    }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
-	{
-		return [
-			'title' => 'required',
-            'description' => 'required'
-		];
-	}
+    /**
+    * Get the validation rules that apply to the request.
+    *
+    * @return array
+    */
+    public function rules(){
+        return [
+            'title' => 'required|unique|max:30',
+            'description' => 'required',
+            'start_at' => 'required',
+            'end_at' => 'required',
+            'isAllDay' => 'required',
+            'max_people' => 'required',
+        ];
+    }
 }
