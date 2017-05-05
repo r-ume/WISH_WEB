389bb43ff895e32d71c6c571ab1e453e8968c35f
r-ume <baymax.blackandwhite@gmail.com>
2017-05-05 13:30:33 +0000

    made fundamental timeline in the view

--- resources/views/users/myprofile.blade.php
+++ resources/views/users/myprofile.blade.php
@@ -22,29 +22,66 @@
     <div class = "one wide column"></div>
     <div class = "eight wide column" align="center">
         <br />
-        <div class = "infinite-scroll">
+        <div class = "ui feed">
+            @foreach($initial_timeline as $tl)
+                <div class="event">
+                    <div class ="label">
+                        @if($tl->author->image) <img src= {{ $tl->author->image }}>
+                        @elseif($tl->creator->image) <img src= {{ $tl->author->image }}>
+                        @endif
+                    </div>
 
-        <div class="ui feed">
-                @foreach($events as $event)
+                    <div class="content">
+                        <div class="summary">
+                            <a>{{ $tl->author->first_name }} {{ $tl->author->last_name }}</a> made a new {{ get_class($tl) }}
+                            <div class="date">{{ $tl->created_at }}</div>
+                        </div>
+
+                        <div class="extra text">
+                            @if($tl->description)
+                                <a href = "{{action('EventsController@show', [$tl->id])}}" style = "color: black">
+                                    {{ $tl->description }}</a>
+                            @elseif($tl->content)
+                                <a href = "{{action('EventsController@show', [$tl->id])}}" style = "color: black">
+                                    {{ $tl->content }}</a>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+        <div class = "infinite-scroll">
+            <div class="ui feed">
+                @foreach($timeline as $tl)
                     <div class="event">
                         <div class="label">
-                            <img src= {{ $event->creator->image }}>
+                            id: {{$tl->id}}
+                            class: {{ get_class($tl) }}
+                            {{$tl->created_at}}
+                            {{--@if($tl->creator->image)--}}
+                                {{--<img src= {{ $tl->creator->image }}>--}}
+                            {{--@elseif($tl->author->image)--}}
+                                {{--<img src= {{ $tl->creator->image }}>--}}
+                            {{--@else--}}
+                                {{--no photo--}}
+                            {{--@endif--}}
                         </div>
-                        <div class="content">
-                            <div class="summary">
-                                <a>{{ $event->creator->first_name }} {{ $event->creator->last_name }}</a> made a new status
-                                <div class="date">{{ $event->created_at }}</div>
-                            </div>
-                            <div class="extra text">
-                                <a href = "{{action('EventsController@show', [$event->id])}}" style = "color: black">
-                                    {{ $event->description }}
-                                </a>
-                            </div>
-                        </div>
+                        {{--<div class="content">--}}
+                            {{--<div class="summary">--}}
+                                {{--<a>{{ $event->creator->first_name }} {{ $event->creator->last_name }}</a> made a new status--}}
+                                {{--<div class="date">{{ $event->created_at }}</div>--}}
+                            {{--</div>--}}
+                            {{--<div class="extra text">--}}
+                                {{--<a href = "{{action('EventsController@show', [$event->id])}}" style = "color: black">--}}
+                                    {{--{{ $event->description }}--}}
+                                {{--</a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
                     </div>
                     <br>
                 @endforeach
-                {{ $events->links() }}
+                {{ $timeline->links() }}
             </div>
         </div>
     </div>
